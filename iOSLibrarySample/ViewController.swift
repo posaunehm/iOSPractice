72ab9316e2c48bb64c57fc2fa87e88a5279e0acd
posaunehm <posaunehm@gmail.com>
2019-01-12 13:51:33 +0900

    Initial Commit

---
+++ iOSLibrarySample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLibrarySample
+//
+//  Created by Maekawa Hiroshi on 2019/01/12.
+//  Copyright © 2019年 Maekawa Hiroshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
